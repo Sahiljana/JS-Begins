61642f14c71cee64aa28c39954c1aba825705c40
Sahiljana <141487578+Sahiljana@users.noreply.github.com>
2025-05-29 17:39:47 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // Treat karta h saara code ko as new JS accordingly
+
+// Code readability should always be high..
+
+let name = "Sahil"
+let age = 18
+let loggedIn = true
+
+console.log(name);
+console.log(3+3);
+
+
+/*
+number => 2 to power 53
+bigint => for large numbers
+string => "" (Enclosed within double quotes)
+boolean => True/False
+null => Ek standalone value hoti h
+undefined =>  kindda place holder for a value which is not defined yet
+symbols => used for uniqueness
+*/
+
+console.log(typeof age);
+console.log(typeof null);  // iska type object hota h jo ki kaafi ajeeb h
+console.log(typeof undefined); // iska type undefined hi hota h
+
