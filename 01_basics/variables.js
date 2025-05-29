263b38281fdd5ea613552a0b107c0dea7be0018a
Sahiljana <141487578+Sahiljana@users.noreply.github.com>
2025-05-29 17:01:15 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const accountId = 1234
+let accountMail = "sahiljana@gmail.com"
+var accountPass = "12345" // Hum isse use nhi karte kyu ki ismei issues aate h, mostly use karte h "let"
+accountState = "jaipur" // Yeh bhi ek tareeka h but not very recommended
+
+let accountHello;
+
+// accountId = 2345 Not possible kyuki humne const use kiya h jo ki change nhi hota..
+
+accountMail = "sahiljana298@gmail.com"
+accountPass = "45678"
+accountState = "Delhi"
+
+console.log(accountId);
+
+console.table([accountId, accountMail, accountPass, accountState, accountHello]);
