1b0e2f9c4faddac2a8f9c414f1747d7b3737cfcc
Sahiljana <141487578+Sahiljana@users.noreply.github.com>
2025-05-31 17:47:10 +0000

    comparsions

---
+++ 01_basics/comparisions.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+// ===  => yeh strict use hota h for comparision and check karta h datatype ko bhi
+
+console.log("2" === 2);
