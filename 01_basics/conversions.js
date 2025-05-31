f65b9427ddc30056b0ca8b0f8a9426519c8d05e4
Sahiljana <141487578+Sahiljana@users.noreply.github.com>
2025-05-31 10:43:44 +0000

    conversions

---
+++ 01_basics/conversions.js
@@ -0,0 +1,34 @@
+let score = 33
+
+//console.log(score);
+let numberconv = Number(score)
+
+//console.log(typeof numberconv);
+//console.log(numberconv);
+
+let numbertostringg = String(score)
+//console.log(typeof numbertostringg);
+//console.log(numbertostringg);
+
+let scored = "33abc"
+console.log(typeof scored);
+let scoredconv = Number(scored)
+console.log(typeof scoredconv);
+console.log(scoredconv);
+
+
+let booleans = 1
+let convert = Boolean(booleans)
+console.log(convert);
+console.log(typeof convert);
+
+
+// Aise hi Boolean , String wagera work karti h conversions
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+// 1 => true; 0 => false
+// "" => false. empty ka case mei boolean ka work
+// "sahil" => true. random ke case mei
