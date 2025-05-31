914fedf8cd650ebae8632e354497618d9d5323c5
Sahiljana <141487578+Sahiljana@users.noreply.github.com>
2025-05-31 11:12:50 +0000

    operations

--- 01_basics/conversions.js
+++ 01_basics/conversions.js
@@ -11,16 +11,16 @@ let numbertostringg = String(score)
 //console.log(numbertostringg);
 
 let scored = "33abc"
-console.log(typeof scored);
+//console.log(typeof scored);
 let scoredconv = Number(scored)
-console.log(typeof scoredconv);
-console.log(scoredconv);
+//console.log(typeof scoredconv);
+//console.log(scoredconv);
 
 
 let booleans = 1
 let convert = Boolean(booleans)
-console.log(convert);
-console.log(typeof convert);
+//console.log(convert);
+//console.log(typeof convert);
 
 
 // Aise hi Boolean , String wagera work karti h conversions
@@ -32,3 +32,44 @@ console.log(typeof convert);
 // 1 => true; 0 => false
 // "" => false. empty ka case mei boolean ka work
 // "sahil" => true. random ke case mei
+
+
+// -------------- Operations ---------------
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+//    console.log(true);
+//  console.log(+true); Not recommended just for understanding => o/p = 1
+//    console.log(+"");  => o/p = 0
+
+let gameCounter = 100
+gameCounter++ // postfix
+console.log(gameCounter);
+++gameCounter // prefix
+console.log(gameCounter);
+
+//go to mdn js postfix and orefix to study these
+
+
+
+
+
+
+
+
+
